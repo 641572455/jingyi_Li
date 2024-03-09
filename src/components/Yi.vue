6eb067b4845909a1846795a959ae164f8b383971
jingyi li <641572455@qq.com>
2024-03-09 13:19:50 +0800

    four

---
+++ src/components/Yi.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+     {{ message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    messgae: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
